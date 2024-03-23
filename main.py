38e8c43427e8bdb4631a764e72c209f09fe86d77
Divesh Jadhwani <113240252+d-hackmt@users.noreply.github.com>
2024-03-23 16:37:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -61,12 +61,10 @@ def main():
             st.subheader("Binary Threshold Image")
             st.image(binary_image, channels="GRAY")
         
-        # Prepare the image for classification
+       # Prepare the image for classification
         resized_image = cv2.resize(binary_image, (224, 224))
         normalized_image_array = (resized_image.astype(np.float32) / 127.5) - 1
         data = np.expand_dims(normalized_image_array, axis=0)  # Add an extra dimension to represent batch size
-        data = np.stack((data,) * 3, axis=-1)  # Stack the array to have shape (1, 224, 224, 3)
-
         
         # Predict using the model
         prediction = model.predict(data)
